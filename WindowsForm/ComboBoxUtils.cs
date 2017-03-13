aad0e0e363d69e7ad8a8b240f68670f9ec1ed443
grandiloquent <1785907865@qq.com>
2017-03-13 20:45:08 +0800

    changed

---
+++ WindowsForm/ComboBoxUtils.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Utilities.WindowsForm
+{
+    public static class ComboBoxUtils
+    {
+
+        public static void LoadItemsFromFile(this ToolStripComboBox box, string fileName)
+        {
+
+            var p = fileName.GetApplicationPath();
+
+            if (p.IsFile())
+            {
+                var ls = p.ReadFileAllLines().Where(i => !i.IsNullOrWhiteSpace()).OrderBy(i => i);
+
+                box.Items.Clear();
+                box.Items.AddRange(ls.ToArray());
+            }
+
+        }
+        public static void AddItemsToFile(this ToolStripComboBox box, string fileName, string value)
+        {
+
+            var p = fileName.GetApplicationPath();
+
+            if (p.IsFile())
+            {
+                var ls = p.ReadFileAllLines().ToList();
+
+                if (ls.Contains(value)) return;
+                else
+                {
+                    ls.Add(value);
+                }
+
+                using (var writer = new StreamWriter(p, true, FileUtils.UTF8_ENCODING))
+                {
+                    writer.Write(value);
+                    writer.Write(Environment.NewLine);
+                }
+
+                box.Items.Clear();
+                box.Items.AddRange(ls.ToArray());
+            }
+            else
+            {
+                value.StringToFile(p);
+                box.Items.Add(value);
+                
+            }
+
+        }
+    }
+}
